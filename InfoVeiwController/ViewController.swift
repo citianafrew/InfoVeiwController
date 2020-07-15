4911e6a51d0a93b51776a255f1a06d69d7b0db6b
Citiana Frew <citianaafrew@gmail.com>
2020-07-14 23:46:30 -0400

    Initial Commit

---
+++ InfoVeiwController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InfoVeiwController
+//
+//  Created by citiana frew on 7/14/20.
+//  Copyright © 2020 citiana frew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
